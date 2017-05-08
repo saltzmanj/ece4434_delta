2fa9dbebf874ab1430cf117038c520d9e63e2919
Jake Saltzman <jss7kj@virginia.edu>
2017-05-08 15:16:16 -0400

    fixed check sort

--- assembler/sample_programs/bubblesort/bubblesort_check.asm
+++ assembler/sample_programs/bubblesort/bubblesort_check.asm
@@ -31,19 +31,19 @@ CHK INST_CHECK 3
 ld r6, rax, 0	; Load array size into r6 ( i = LENGTH ) 0x36
 mov r9, r6
 add r9, r9 		; basically a comparison (<- outer loop target)
-bz 21				; If i is zero, branch out of the outer loop (i > 0)
+bz 24				; If i is zero, branch out of the outer loop (i > 0)
 lil r7, 0 		; (j = 0) (<- inner loop init)
 mov r8, r6 		; (<- inner loop target)
 sub r8, r11 	; r8 = i - 1
 sub r8, r7 		; i-1 - j
-bz 	14			; if j = i - 1, exit inner loop
+bz 	17			; if j = i - 1, exit inner loop
 				; TODO: Calculate branch addresses
 				; Main swap logic
 mov rdi, rsi
 add rdi, r7
 chk INST_CHECK, 1
 ld rcx, rdi, 0 ; Load mem[j] = rcx, mem[j+1] = rdx
-; chk INST_CHECK, 2
+chk INST_CHECK, 2
 ld rdx, rdi, 1
 mov r9, rcx
 mov r10, rdx
@@ -57,9 +57,9 @@ sub r8, r11
 st rdx, rdi, 0
 
 add r7, r11 ; add 1 to j (j++)
-br -16; go back to inner loop start
+br -19; go back to inner loop start
 sub r6, r11 ; Subtract 1 from i (i--)
-br -22; go back to outer loop start
+br -24; go back to outer loop start
 nop
 [FILL] 0x0000, 0x100 ; Start of check table
 st rcx, rdi, 1
